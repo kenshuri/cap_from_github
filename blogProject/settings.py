157506910089309ed2dc632463fbe99017d66cee
Alexis <kenshuri@hotmail.fr>
2022-08-26 18:17:41 +0200

    Add SECRET_KEY in .env

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -15,16 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%&qzlpn#0n8bmy+8-m5!e6vnp!k_q#i)!_1c-csr*vr_lcy&xh'
-
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+
 DEBUG = os.environ.get("DJANGO_DEBUG") == 'True'
 
 ALLOWED_HOSTS = ['deploy-to-heroku-blog-example.herokuapp.com']
